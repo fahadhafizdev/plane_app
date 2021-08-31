5c7e89eea81178305b73c55d683a81dc449ec497
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-08-31 19:14:01 +0700

    Create choose_seat_page.dart

---
+++ lib/ui/pages/choose_seat_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:plane_app/shared/theme.dart';
+
+class ChooseSeatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('choose seat page'),
+      ),
+    );
+  }
+}
