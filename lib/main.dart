1fbe708aaeb14f8970a494a83be4eacc491b8851
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-08 12:17:11 +0700

    sign in page prepare

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plane_app/ui/pages/sign_in_page.dart';
 
 import 'package:plane_app/ui/pages/splash_page.dart';
 import 'package:plane_app/ui/pages/get_started_page.dart';
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
           '/': (context) => SplashPage(),
           '/get-started': (context) => GetStartedPage(),
           '/sign-up': (context) => SignUpPage(),
+          '/sign-in': (context) => SignInPage(),
           '/bonus-page': (context) => BonusPage(),
           '/main': (context) => MainPage(),
         },
---
+++ lib/ui/pages/sign_in_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:plane_app/shared/theme.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
