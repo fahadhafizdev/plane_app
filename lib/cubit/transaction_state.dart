10fc343759b13c96c55c1534a5d4351bbaeded8d
fahadhafiz dev <fahadhafiz.dev@gmail.com>
2021-09-15 16:56:28 +0700

    transaction cubit and state initial

---
+++ lib/cubit/transaction_state.dart
@@ -0,0 +1,22 @@
+part of 'transaction_cubit.dart';
+
+abstract class TransactionState extends Equatable {
+  const TransactionState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class TransactionInitial extends TransactionState {}
+
+class TransactionLoading extends TransactionState {}
+
+class TransactionFailed extends TransactionState {
+  final String error;
+
+  TransactionFailed(this.error);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [error];
+}
